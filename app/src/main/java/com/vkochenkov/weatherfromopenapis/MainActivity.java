c20ffec3d3a41100ecb270bafe68210f03a5415a
Vlad Kochenkov <vkochenkov@alfabank.ru>
2019-09-30 17:58:38 +0300

    added geolocation

--- app/src/main/java/com/vkochenkov/weatherfromopenapis/MainActivity.java
+++ app/src/main/java/com/vkochenkov/weatherfromopenapis/MainActivity.java
@@ -1,7 +1,15 @@
 package com.vkochenkov.weatherfromopenapis;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -37,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
     MainResponseObject mainResponseObject;
 
     // локальные переменные для параметров
-    private String localTime;
     private String temperatureCel;
     private String pressure;
     private String humidity;
@@ -45,6 +52,9 @@ public class MainActivity extends AppCompatActivity {
     private Double humidityPercent;
     private Double pressurePa;
     private Double pressureMm;
+
+
+    LocationManager locationManager;
 
 
     @Override
@@ -56,73 +66,74 @@ public class MainActivity extends AppCompatActivity {
         longitudeField = findViewById(R.id.edt_longitude);
         latitudeField = findViewById(R.id.edt_latitude);
         iconViewImage = findViewById(R.id.img_view_icon);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
     }
 
     public void getParamsFromFields() {
         LATITUDE = latitudeField.getText().toString();
         LONGITUDE = longitudeField.getText().toString();
-        //todo add parser for change "," to "."
     }
 
     // основной метод отправки запроса и приема ответа
     public void getWeatherFromApi() {
         WeatherApiManager
-            .getRequest()
-            .getWeather(KEY, LATITUDE, LONGITUDE, UNITS)
-            .enqueue(new Callback<MainResponseObject>() {
-                @Override
-                public void onResponse(Call<MainResponseObject> call, Response<MainResponseObject> response) {
-                    if (!response.isSuccessful()) {
-                        message = "Something went wrong. Response code: " + response.code();
+                .getRequest()
+                .getWeather(KEY, LATITUDE, LONGITUDE, UNITS)
+                .enqueue(new Callback<MainResponseObject>() {
+                    @Override
+                    public void onResponse(Call<MainResponseObject> call, Response<MainResponseObject> response) {
+                        if (!response.isSuccessful()) {
+                            message = "Something went wrong. Response code: " + response.code();
+                            showMessage(message);
+                            hideIcon(icon);
+                            return;
+                        }
+
+                        mainResponseObject = response.body();
+
+                        temperatureCel = mainResponseObject.getCurrently().getTemperature();
+                        humidity = mainResponseObject.getCurrently().getHumidity();
+                        humidityPercent = Double.parseDouble(humidity) * 100;
+                        pressure = mainResponseObject.getCurrently().getPressure();
+                        pressurePa = Double.parseDouble(pressure) * 100; //переводим из строки и из гекто-Паскалей в Паскили
+                        pressureMm = Math.ceil(pressurePa / 133.3); //переводим в мм.рт.ст.
+                        icon = response.body().getCurrently().getIcon(); //для теста
+
+                        message =
+                                "Заданная область:" + "\n" +
+                                        "широта: " + mainResponseObject.getLatitude() + "°;" + "\n" +
+                                        "долгота: " + mainResponseObject.getLongitude() + "°;" + "\n" +
+                                        "тайм-зона: " + mainResponseObject.getTimezone() + "." + "\n" +
+                                        "\n" +
+                                        "Текущая погода в заданной области:" + "\n" +
+                                        "температура: " + temperatureCel + "°C;" + "\n" +
+                                        "влажность: " + humidityPercent + "%;" + "\n" +
+                                        "давление: " + pressureMm + "  мм.рт.ст.;" + "\n" +
+                                        "скорость ветра: " + response.body().getCurrently().getWindSpeed() + " м/с;" + "\n" +
+                                        "общий прогноз: " + response.body().getCurrently().getSummary() + "." + "\n";
+
                         showMessage(message);
-                        return;
+                        showIcon(icon);
+
+                        //todo добавить погоду на завтра
                     }
 
-                    mainResponseObject = response.body();
-
-                    //localTime = getLocalTime(); - не будет использоваться
-                    temperatureCel = mainResponseObject.getCurrently().getTemperature();
-                    humidity = mainResponseObject.getCurrently().getHumidity();
-                    humidityPercent = Double.parseDouble(humidity)*100;
-                    pressure = mainResponseObject.getCurrently().getPressure();
-                    pressurePa = Double.parseDouble(pressure)*100; //переводим из строки и из гекто-Паскалей в Паскили
-                    pressureMm = Math.ceil(pressurePa/133.3); //переводим в мм.рт.ст.
-                    icon = response.body().getCurrently().getIcon(); //для теста
-
-                    message =
-                            "Текущая погода в заданной области" + "\n" +
-                            "Широта: " + mainResponseObject.getLatitude() + "°" + "\n" +
-                            "Долгота: " + mainResponseObject.getLongitude() + "°" + "\n" +
-                            "Тайм-зона: " + mainResponseObject.getTimezone() + "\n" +
-                            "Температура: " + temperatureCel + "°C" + "\n" +
-                            "Влажность: " + humidityPercent + "%" + "\n" +
-                            "Давление: " + pressureMm + "  мм.рт.ст." + "\n" +
-                            "Скорость ветра: " + response.body().getCurrently().getWindSpeed() + " м/с" + "\n" +
-                            "Общий прогноз: " + response.body().getCurrently().getSummary()  + "\n";
-                    showMessage(message);
-
-                    showIcon(icon);
-
-                    //todo добавить запрос местоположения девайса
-
-                    //todo добавить погоду на завтра
-                }
-
-                @Override
-                public void onFailure(Call<MainResponseObject> call, Throwable t) {
-                    message = t.getMessage();
-                    showMessage(message);
-                }
-            });
+                    @Override
+                    public void onFailure(Call<MainResponseObject> call, Throwable t) {
+                        message = t.getMessage();
+                        showMessage(message);
+                        hideIcon(icon);
+                    }
+                });
     }
 
-    // нажатие на кнопку
     public void getWeather(View view) throws InterruptedException {
         getParamsFromFields();
         getWeatherFromApi();
+
     }
 
-    // нажатие на кнопку
     public void setSPbLocation(View view) {
         latitudeField.setText("59.939095");
         longitudeField.setText("30.315868");
@@ -130,12 +141,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void showMessage(String message) {
         textViewResult.setText(message);
+        textViewResult.setVisibility(textViewResult.VISIBLE);
     }
-
-//    public String getLocalTime() {
-//        Date date = new Date();
-//        return date.toString();
-//    }
 
     public void showIcon(String icon) {
         switch (icon) {
@@ -173,9 +180,50 @@ public class MainActivity extends AppCompatActivity {
                 //todo
                 break;
         }
+        iconViewImage.setVisibility(iconViewImage.VISIBLE);
     }
 
+    public void hideIcon(String icon) {
+        iconViewImage.setVisibility(iconViewImage.INVISIBLE);
+    }
+
+    //код для получения локации
+    //todo понять почему лос-анжелес >__<
     public void setLocation(View view) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Проверка наличия разрешений
+            // Если нет разрешения на использование соответсвующих разркешений выполняем какие-то действия
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,listener);
+
+        locationManager.requestSingleUpdate (LocationManager.GPS_PROVIDER, listener, null);
 
     }
+
+    private LocationListener listener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            if (location!=null) {
+                latitudeField.setText(String.valueOf(location.getLatitude()));
+                longitudeField.setText(String.valueOf(location.getLongitude()));
+            }
+            else{
+                latitudeField.setText("Sorry, location");
+                longitudeField.setText("unavailable");
+            }
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+    };
 }
